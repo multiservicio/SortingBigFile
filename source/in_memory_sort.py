a44e8b1520c1946e15f91fb58323309b1fd57c8f
Pedro Diaz <pedro.diaz@kiosked.com>
2016-09-17 12:22:21 +0300

    Add bubble sort as in memory sorting Add simple test

--- source/in_memory_sort.py
+++ source/in_memory_sort.py
@@ -1,9 +1,23 @@
 
 
+# In Memory Sort is a Class intented to sort the lines that fit into memory.
+# For this example Bubble Sort is the selected for its simplicity on the implementation
 class InMemorySort:
 
-    # Static method sort
-    # TODO
+    # Static method since there is no need to instanciate an object
+    # Bubble Sort is one of the basic sorting algorithm
+    # https://en.wikipedia.org/wiki/Bubble_sort
     @staticmethod
-    def sort(data):
-        pass
+    def bubble_sort(data: list) -> list:
+        for interval in range(len(data) - 1, 0, -1):
+            # Range(interval) represents the index values from beginning to the interval of the list
+            for index in range(interval):
+                # If pos[0] bigger (alphabetically) than pos[0 + 1]
+                if data[index] > data[index + 1]:
+                    # Then swap positions
+                    temp = data[index]
+                    data[index] = data[index + 1]
+                    data[index + 1] = temp
+
+        return data
+
---
+++ test/test_in_memory_sort.py
@@ -0,0 +1,21 @@
+import unittest
+
+from source.in_memory_sort import InMemorySort
+
+
+class TestInMemorySort(unittest.TestCase):
+    def setUp(self):
+        self.test_chunk_of_lines = [
+            "promotrix curialism walk triploidite diluvion toddle nucleoalbumin disestablishmentarian redberry lapsi Hitchiti gladsome prehatred hatchettine herem unworthily",
+            "vociferousness subterrene pseudoperspective nonfuroid apogaeic ratchment coltpixie temporale betrothed enzymatic undergraduate palp screwmatics Cystopteris Tragopogon ramroddy",
+            "nesquehonite pah damewort acnemia papyrology cantilena hacked anagrammatist santir Ribhus ichnomancy marrowy tidehead Pteropidae Coniferae dayflower",
+        ]
+        self.expected_sorted_lines = [
+            "nesquehonite pah damewort acnemia papyrology cantilena hacked anagrammatist santir Ribhus ichnomancy marrowy tidehead Pteropidae Coniferae dayflower",
+            "promotrix curialism walk triploidite diluvion toddle nucleoalbumin disestablishmentarian redberry lapsi Hitchiti gladsome prehatred hatchettine herem unworthily",
+            "vociferousness subterrene pseudoperspective nonfuroid apogaeic ratchment coltpixie temporale betrothed enzymatic undergraduate palp screwmatics Cystopteris Tragopogon ramroddy"
+        ]
+
+    def test_bubble_sort(self):
+        sorted_lines = InMemorySort.bubble_sort(self.test_chunk_of_lines)
+        self.assertEquals(self.expected_sorted_lines, sorted_lines)
