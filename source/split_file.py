9bd3b93abd320d9a1af028c3abb1af14172377cc
Pedro Diaz <pedro.diaz@kiosked.com>
2016-09-17 19:09:47 +0300

    Split class now splitting and sorting

--- source/split_file.py
+++ source/split_file.py
@@ -1,4 +1,4 @@
-from in_memory_sort import InMemorySort
+from source.in_memory_sort import InMemorySort
 
 
 # Split the given file into a smaller-sorted one.
@@ -6,17 +6,19 @@ from in_memory_sort import InMemorySort
 # and perform in memory sort.
 class SplitFile:
 
-    # Class variable that defines the naming for this partial files
-    PARTIAL_FILE_NAME = 'split_{0}.partial'
-
     # When creating an instance of this object we will pass the file name
-    def __init__(self, file_name):
+    # File name of the file to split
+    # Chunk size (in Bytes) of the amount of bytes to read each time
+    # This should be big enough and fit in memory
+    # Partial file name for the new created partial sorted files
+    def __init__(self, file_name: str, chunk_size: int, partial_file_name: str = 'split_{0}.partial'):
         self.file_name = file_name
+        self.chunk_size = chunk_size
+        self.partial_file_name = partial_file_name
 
     # Main functionality of this class. To split given file into smaller sorted ones
     # In order to split and sort we will doing the following
-    # Read a bunch of lines (until the given chunk size, typically the amount of memory to use)
-    def split(self, chunk_size):
+    def split(self):
         # Since over this file we do not need any other operation than read
         file = open(self.file_name, 'r')
 
@@ -28,15 +30,17 @@ class SplitFile:
         do_we_continue = True
         partial_file_number = 0
         while do_we_continue:
-            block_of_lines = file.readlines(chunk_size)
+            # Chunk size is the size to read from the file 'at once'
+            # this is size hint
+            # More info http://stackoverflow.com/questions/14541010/pythons-function-readlinesn-behavior
+            block_of_lines = file.readlines(self.chunk_size)
 
             # We reached the end of the file
             if not block_of_lines:
                 do_we_continue = False
 
-            # Sort the lines in memory using --- sorting algorithm
-            sorted_lines = InMemorySort.sort(block_of_lines)
-            # block_of_lines.sort()
+            # Sort the lines in memory using Bubble Sort sorting algorithm
+            sorted_lines = InMemorySort.bubble_sort(block_of_lines)
 
             # Save into partial file the sorted lines
             self._dump_into_partial_file(sorted_lines, partial_file_number)
@@ -49,9 +53,11 @@ class SplitFile:
     # The names of such files is defined in the class variable PARTIAL_FILE_NAME
     # Been of the format:
     #   split_0.partial -> Containing the first block of lines
-    def _dump_into_partial_file(self, chunk_of_lines, partial_file_identifier):
+    def _dump_into_partial_file(self, chunk_of_lines: list, partial_file_identifier: str):
         # As we know that the variable is string we can substitute the value
         # https://docs.python.org/3.4/library/stdtypes.html#str.format
-        file = open(self.PARTIAL_FILE_NAME.format(partial_file_identifier), 'w')
-        file.write(chunk_of_lines)
+        file = open(self.partial_file_name.format(partial_file_identifier), 'w')
+        # Note: on writing we use the empty string and join since the input is a list
+        # And we need to write it in the file as lines.
+        file.write(''.join(chunk_of_lines))
         file.close()
---
+++ test/test_split_file.py
@@ -0,0 +1,12 @@
+import unittest
+
+from source.split_file import SplitFile
+
+
+class TestSplitFile(unittest.TestCase):
+    def setUp(self):
+        size_in_bytes_of_each_partial = 10 * 1024
+        self.split_object = SplitFile('test_split_unsorted.txt', size_in_bytes_of_each_partial)
+
+    def test_split_file_in_partial_sorted_files(self):
+        self.split_object.split()
