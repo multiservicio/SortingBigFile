8e9b938127bb321ee10ab12aa295fbf974fa4a95
Pedro Diaz <pedro.diaz@kiosked.com>
2016-09-17 20:32:34 +0300

    start the merge class

--- source/external_sorting.py
+++ source/external_sorting.py
@@ -11,29 +11,31 @@ class ExternalSorting:
 
     # When creating an instance of this object we must specify the size of the chunk
     # Been the chunk the amount of memory use to chop our big file.
-    def __init__(self, chunk_size: int, partial_file_name: str = 'split_{0}.partial'):
+    def __init__(self, chunk_size: int, file_name: str, partial_file_name: str = 'split_{0}.partial'):
         self.chunk_size = chunk_size
+        self.file_name = file_name
         self.partial_file_name = partial_file_name
 
-    # Sort is the public method that performs the literal sorting algorightm.
+    # Sort is the public method that performs the literal sorting algorithm.
     # In this case the flow is the following
     # We split the big file into smaller files (that they can fit on memory).
     # They need to fit in memory so we can sort each "piece" faster
     # Then we have to merge all this files until we have again a single final file
     # That is sorted.
-    def sort(self, file_name: str):
-        # First we need to know how big is the file and then calculate the number of "divisions" of it
-        # We will put this into a "private" method
-        number_of_partial_files = self._get_number_of_partial_files(file_name)
+    def sort(self):
 
         # Next will be the actual split of the big file.
         # Each partial file will be already sorted within itself.
         split_files = SplitFile(
-            file_name=file_name,
+            file_name=self.file_name,
             chunk_size=self.chunk_size,
             partial_file_name=self.partial_file_name
         )
         split_files.split()
+
+        # First we need to know how big is the file and then calculate the number of "divisions" of it
+        # We will put this into a "private" method
+        number_of_partial_files = split_files.get_amount_of_partial_files()
 
         # Now we have the file already divided in number_of_partial_files and each of the partials
         # are already sorted within themselves.
@@ -56,4 +58,4 @@ class ExternalSorting:
         # Plus 1 for the rest (in case is the division is not exact)
         number_of_partial_files = (file_size / self.chunk_size) + 1
 
-        return number_of_partial_files
+        return int(number_of_partial_files)
--- source/merge_file.py
+++ source/merge_file.py
@@ -3,8 +3,15 @@
 # Merge File is the class where the responsibility of merging the partial files into
 # a sorted merged one
 class MergeFile:
-    def __init__(self):
-        pass
 
+    # When creating an instance of this object we will pass the amount of partial files.
+    # The reason of this is to be able to merge those files
+    # NOTE: This is a naive approach and assumes the folder only contains the correct files
+    def __init__(self, total_amount_of_partial_files: int, partial_file_name: str = 'split_{0}.partial'):
+        self.partial_files = total_amount_of_partial_files
+        self.partial_file_name = partial_file_name
+
+    # This is the main method. Here is implemented the merging of the partial files into
+    # a single one.
     def merge(self):
         pass
